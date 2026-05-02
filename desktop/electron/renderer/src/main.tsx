f63e9e1dc0814e04b865b28d37c6746d05f84c44
TianmingLe <TianmingLe@users.noreply.github.com>
2026-05-02 06:13:43 +0000

    feat(renderer): wrap AppRouter with I18nProvider

--- desktop/electron/renderer/src/main.tsx
+++ desktop/electron/renderer/src/main.tsx
@@ -2,6 +2,7 @@ import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import { ErrorBoundary } from './components/error/ErrorBoundary'
 import { InlineSkeleton } from './components/error/InlineSkeleton'
+import { I18nProvider } from './i18n/I18nProvider'
 import './styles.css'
 
 const AppRouter = React.lazy(() => import('./app/router').then((m) => ({ default: m.AppRouter })))
@@ -10,7 +11,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ErrorBoundary>
       <Suspense fallback={<InlineSkeleton />}>
-        <AppRouter />
+        <I18nProvider>
+          <AppRouter />
+        </I18nProvider>
       </Suspense>
     </ErrorBoundary>
   </React.StrictMode>
